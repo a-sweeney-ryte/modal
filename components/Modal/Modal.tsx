9975a0d4a0b7b847c9ab096b00de2e384d0b450f
Alex Sweeney <a.sweeney@ryte.com>
2022-04-07 16:39:39 +0100

    stop scrolling when Modal open

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,16 +1,54 @@
-import styles from '../../styles/Modal.module.css';
+import styles from './Modal.module.css';
+import {useState, useEffect} from 'react';
 
 export const Modal = ({
   children,
 }) => { 
-  return (
-    <div className={styles.overlay}>
-      <div className={styles.wrapper}>
-        <div className={styles.closeButtonContainer}>
-          <button className={styles.closeButton}>x</button>
-        </div>
-        {children}
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const disableScroll = () => { 
+    document.body.style.height = '100%';
+    document.body.style.overflow = 'hidden'; 
+  }
+
+  const enableScroll = () => { 
+    document.body.style.height = 'inherit';
+    document.body.style.overflow = 'overlay'; 
+  }
+
+  useEffect(() => {
+    if(isOpen) {
+      disableScroll()
+    } else {
+      enableScroll()
+    }
+
+    return () => {
+      enableScroll()
+    }
+  }, [isOpen])
+
+
+  return ( 
+    <>
+      <div className={styles.openButtonContainer}>
+        <button className={styles.openButton} onClick={handleButtonClick}>Open Modal</button>
       </div>
-    </div>
+
+      { isOpen && 
+        <div className={styles.overlay}>
+          <div className={styles.wrapper}>
+            <div className={styles.closeButtonContainer}>
+              <button className={styles.closeButton} onClick={handleButtonClick}>x</button>
+            </div>
+            {children}
+          </div>
+        </div>
+      }
+    </>
   )
-}
+} 
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
 import styles from '../styles/Home.module.css'
 import { Modal } from '../components/Modal/Modal.tsx';
+import { useState } from 'react';
 
 export default function Home() {
   return (
-    <div className={styles.background}> 
-      <h1 className={styles.heading}>Example Heading</h1>
-      <p className={styles.text}>example text</p>
+    <div className={styles.wrapper}>
+      <div className={styles.hero}> 
+        <h1 className={styles.heading}>Example Heading</h1>
+        <p className={styles.text}>example text</p>
+ 
+        <Modal>
+          <p>Hello World</p>
+        </Modal> 
+      </div>
 
-      <div className={styles.btnContainer}>
-        <button className={styles.btn}>Open Modal</button>
-      </div>   
-
-      <Modal>
-        <p>Hello World</p>
-      </Modal> 
+      <div className={styles.content}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </div>
   )
 }
